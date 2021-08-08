5611342a9601b8d86ea71696bc1ea83a3f6b10a7
rick0048 <rick0048@algonquinlive.com>
2021-08-07 23:48:49 -0400

    changed file structure, added get route for api/notes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -7,23 +7,21 @@ const PORT = 3001
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../', '../', 'notes.html'))
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', '../', '../', '../', 'index.html'))
-});
+})
 
 app.get('/api/notes', (req, res) => {
-    // fs.readFile('db.json', (err, data) => {
-    //     if (err) {
-    //         console.log(`The error is ${err}`)
-    //     }
+    fs.readFile('../../../db/db.json', (err, data) => {
+        if (err) {
+            console.log(`The error is ${err}`)
+        }
+        res.send(data)
+    })
+})
 
-        
-    // }) 
-    res.send('data')
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', '../', 'index.html'))
 })
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
-});
+})
